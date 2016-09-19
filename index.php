1bc1f73590443763d0017cd76bdcc8da175e4869
Ajit Das <ajitdas2900@gmail.com>
2016-09-19 13:10:59 +0600

    all file added

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Web App Develop-PHP
+ * Date: 9/7/2016
+ * Time: 11:03 AM
+ */
+
+echo "First Project of Ajit";
+
+echo "<br>";
+echo "First Project of Ajit is edited at home";
+<<<<<<< HEAD
+
+=======
+echo "First Project of Ajit is edited at home 2"
+>>>>>>> d6f73b7cae6e0ae96990218b51a5bc22a233df95
